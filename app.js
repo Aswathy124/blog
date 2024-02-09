c236908679cff040834c792b7363d36826f1f81b
Aswathy124 <aswathykannan523@gmail.com>
2024-02-09 14:48:40 +0530

    blog

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express")
 const cors=require("cors")
 const mongoose=require("mongoose")
 const blogRoute=require("./controller/blogRoute")
-
+const postRoute=require("./controller/postRouter")
 const app=express()
 
 app.use(express.json())
@@ -17,6 +17,8 @@ mongoose.connect("mongodb+srv://achu:Harifa123@cluster0.6jzim5t.mongodb.net/blog
 
 
 app.use("/api/blog",blogRoute)
+
+app.use("/api/post",postRoute)
 
 
 app.listen(3001,()=>{
